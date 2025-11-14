ddcd9190291cf98e20518a5119525da7e4e7b51c
unknown <daniel67178@hotmail.com>
2025-11-13 21:29:21 -0600

    Primera version suma

---
+++ Suma/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.Reader;
+import java.util.Scanner;
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        int numero1 = 0;
+        int numero2 = 0;
+
+        System.out.println("Introduce el primer numero");
+        numero1 = reader.nextInt ();
+
+        System.out.println("Introduce el Segundo numero");
+        numero2 = reader.nextInt ();
+
+        int resultado = numero1+numero2;
+
+        System.out.println("El resultado de:"+numero1 + "+" +numero2 + " es:" +resultado);
+    }
+    }
